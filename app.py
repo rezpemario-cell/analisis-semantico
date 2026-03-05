1e9c75fe39a9b1f8c53e0642f42b245b4173aea9
rezpemario-cell <rezpemario@gmail.com>
2026-03-05 11:36:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -498,6 +498,7 @@ elif modo == "🗺️ Cartografía Social":
                         df_result.loc[mask, "lineas_inversion"] = "Sin líneas definidas"
                         continue
 
+                   
                    frases_lote = subset["frase"].tolist()
                     lineas_str = "\n".join(f"- {l}" for l in lineas_disponibles)
                     lineas_validas_set = set(l.lower() for l in lineas_disponibles)
@@ -913,4 +914,5 @@ Frases más representativas:
                         file_name="informe_cartografia.txt",
                         key="descarga_informe_cart"
                     )
+
 
